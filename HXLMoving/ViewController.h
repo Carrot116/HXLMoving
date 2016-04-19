79b09a52cc7615f01aa92b66e9460116e608a1a4
Carrot <Carrot@MacMini.local>
2016-04-19 16:50:55 +0800

    init

---
+++ HXLMoving/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXLMoving
+//
+//  Created by Carrot on 16/4/19.
+//  Copyright © 2016年 Carrot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
